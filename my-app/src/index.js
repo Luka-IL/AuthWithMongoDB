d4b6772b41bb26b71c27ee96a26ea0453a307931
Ilya <dert_94@mail.ru>
2021-02-01 11:57:10 +0300

    new scripts

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import App from './components/app/App';
-//import {Provider} from "react-redux";
-//import {createStore} from "redux";
-//import {composeWithDevTools} from "redux-devtools-extension";
-//import reducer from "./store/reducer/reducer";
 
-//const store = createStore(reducer);
 
 ReactDOM.render(
     <App />,
